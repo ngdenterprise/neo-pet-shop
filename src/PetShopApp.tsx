addbd85cbcd3be22ddb714868bf2e29ba9ab75ad
David Nicholson <me@djnicholson.com>
2021-04-28 19:54:15 -0700

    CenterInScreen

--- src/PetShopApp.tsx
+++ src/PetShopApp.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 
+import CenterInScreen from "./components/CenterInScreen";
+
 import RequireWallet from "./components/RequireWallet/RequireWallet";
 
 function PetShopApp() {
-  return <RequireWallet>Coming soon</RequireWallet>;
+  return (
+    <CenterInScreen>
+      <RequireWallet>The Neo Pet Shop is opening soon!</RequireWallet>
+    </CenterInScreen>
+  );
 }
 
 export default PetShopApp;
